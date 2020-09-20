dfbb84cf6b558e24ff4a3c964a6aef5204793654
Lucas Maciel <lucasmacielgois@gmail.com>
2020-09-19 21:39:48 -0300

    Fazendo requisições em http

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -1,13 +1,30 @@
 import { Course } from './course';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CourseService {
     
-    retrieveAll(): Course[] {
-        return COURSES;
+    constructor(private httpClient: HttpClient){}
+
+    private courseUrl: string = 'http://localhost:3100/api/courses';
+
+    retrieveAll(): Observable<Course[]> {
+        return this.httpClient.get<Course[]>(this.courseUrl);
+    }
+
+    retriveById(id: number): Observable<Course> {
+        return this.httpClient.get<Course>(`${this.courseUrl}/${id}`)
+    }
+    save(course: Course): Observable<Course> {
+        if(course.id){
+            return this.httpClient.put<Course>(`${this.courseUrl}/${course.id}`, course)
+        } else {
+            return this.httpClient.post<Course>(`${this.courseUrl}`, course)
+        }
     }
 }
 var COURSES: Course[] = [

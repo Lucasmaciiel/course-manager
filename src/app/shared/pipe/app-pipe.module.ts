cc0c7c4ccb8d4b8639c2b300a9d3b5cddddb3570
Lucas Maciel <lucasmacielgois@gmail.com>
2020-09-21 18:53:36 -0300

    Criando a pasta shared

---
+++ src/app/shared/pipe/app-pipe.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, Pipe } from '@angular/core';
+import { ReplacePipe } from './replace.pipe';
+
+@NgModule({
+    declarations: [
+        ReplacePipe
+    ],
+    exports: [
+        ReplacePipe
+    ]
+})
+export class AppPipeModule {
+
+}

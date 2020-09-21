fcb2ee1b0872f533211e2c364b45b5e9fe069a57
Lucas Maciel <lucasmacielgois@gmail.com>
2020-09-21 19:01:58 -0300

    Pasta core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 import { RouterModule} from '@angular/router'
 
 import { HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error404/error404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component,
   
   ],
@@ -22,6 +19,7 @@ import { CourseModule } from './courses/course.module';
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses',pathMatch: 'full'
